18cdc850dd216b0178fd2f9ac6ca6db9a2af8e3e
Kane Zhu <me@kane.mx>
2021-11-08 23:19:01 +0800

    chore: gracefully handle the response with error

--- super-resolution.py
+++ super-resolution.py
@@ -39,7 +39,7 @@ def make_signed_request(input_img):
                            aws_service='execute-api')
 
     with open(input_img, 'rb') as image_file:
-        scale = '2'
+        scale = '4'
         payload = {
             'img': base64.b64encode(image_file.read()).decode('utf-8'),
             'scale': scale,
@@ -52,8 +52,12 @@ def make_signed_request(input_img):
         
         original_name = os.path.basename(input_img).split('.')[0]
         if response.status_code >= 200 and response.status_code < 300:
-            im = Image.open(BytesIO(base64.b64decode(response.json()['result'])))
-            im.save(f'{original_name}_x{scale}.png', 'PNG')
+            msg = response.json()
+            if 'result' not in msg:
+                print(msg)
+            else:
+                im = Image.open(BytesIO(base64.b64decode(msg['result'])))
+                im.save(f'{original_name}_x{scale}.png', 'PNG')
         else:
             print(f'Response status is {response.status_code}, and message is {response.text}')
 
